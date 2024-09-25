95179fac755156373599e4e00881b23affc2834a
Deep Patel <W0848895@myscc.ca>
2024-09-25 10:06:19 -0400

    Added App list under Html File and created Input tag

--- src/app/car-list-item/car-list-item.component.ts
+++ src/app/car-list-item/car-list-item.component.ts
@@ -1,14 +1,17 @@
 import { Component , Input } from '@angular/core';
 import { Car } from '../models/car';
-import { NgIf } from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-car-list-item',
   standalone: true,
-  imports: [ NgIf ],
+  imports: [NgIf, NgForOf],
   templateUrl: './car-list-item.component.html',
   styleUrl: './car-list-item.component.css'
 })
+
 export class CarListItemComponent {
  @Input() car?: Car;
+
+
 }
