d533afc1e66083563019ba7515319fb36c381533
Deep Patel <W0848895@myscc.ca>
2024-10-02 12:47:46 -0400

    created return method and added service for dependency injection

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,24 +1,29 @@
 import {Component, OnInit} from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { Car } from '../models/car';
+import { CarListItemComponent } from "../car-list-item/car-list-item.component";
 import { CarService } from '../services/car.service';
 
 @Component({
   selector: 'app-car-list',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf , CarListItemComponent],
 
   templateUrl: './car-list.component.html',
   styleUrls: ['./car-list.component.scss']
 })
 export class CarListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'make', 'model', 'year'];
+  displayedColumns: string[] = ['id', 'make', 'model', 'year' , 'price'];
   carList: Car[] = [];
   constructor(private carService: CarService) {
   }
 
   ngOnInit() {
+    // @ts-ignore
     this.carService.getCars().subscribe({
+      complete: () => console.log("Car data fetch complete!"),
+      error: err => console.error("Error fetching cars", err),
+      next: (data: Car[]) => this.carList = data
     });
   }
 }
